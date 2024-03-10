437e33de2d9a8d2412cf81b769b4c13962836c62
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-10 19:35:45 +0530

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
 
-function App() {
-
-  return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
-    </>
+const App = ()=>{
+  return(
+    <div className="App">
+      <Navbar />
+    </div>
   )
 }
 
-export default App
+export default App;
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Navbar = ()=>{
     return(
         <>
-        
+        <nav>
+          <ul>
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>About</Link>
+            </li>
+            <li>
+            <Link>Services</Link>
+            </li>
+            <li>
+            <Link>Contact</Link>
+
+            </li>
+          </ul>
+        </nav>
         </>
     )
 }
